54f2eabb71f904095b4911388e1483b5643b4e50
zxl <1207346575@qq.com>
2019-09-27 22:27:05 +0800

    微信服务接口

---
+++ ms-shop-service-api/ms-shop-service-api-weixin/src/main/java/com/zxl/weixin/service/WeiXinAppService.java
@@ -0,0 +1,20 @@
+package com.zxl.weixin.service;
+
+import com.zxl.entity.AppEntity;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * 微信服务接口
+ * Created by ZXL on 2019/9/27.
+ */
+@Api(tags = "微信服务接口")
+public interface WeiXinAppService {
+
+    // 功能说明： 应用服务接口
+    @ApiOperation(value = "微信应用服务接口")
+    @GetMapping("/getApp")
+    public AppEntity getApp();
+
+}
